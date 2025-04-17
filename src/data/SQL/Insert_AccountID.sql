b5d50474482fcac8a63a381125349910fddc2821
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-04-17 09:24:04 +0200

    feat(sql): add SQL script for testing to insert a new account into tbl_Account

---
+++ src/data/SQL/Insert_AccountID.sql
@@ -0,0 +1,4 @@
+-- This SQL script inserts a new account into the tbl_Account table.
+-- It is just an example with dummy data.
+INSERT INTO tbl_Account (str_AccountName, str_AccountNumber, real_AccountBalance, real_AccountDifference)
+VALUES ('Sparbuch', '0987654321', 2765.8, +356.5);
