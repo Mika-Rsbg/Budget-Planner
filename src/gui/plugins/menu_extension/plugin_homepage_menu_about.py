7d4067f2e258b6360a735ef227d95964f850b1fd
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-04-24 13:26:29 +0200

    fix(plugin_homepage_menu_about): add Reload option to about menu

--- src/gui/plugins/menu_extension/plugin_homepage_menu_about.py
+++ src/gui/plugins/menu_extension/plugin_homepage_menu_about.py
@@ -8,4 +8,8 @@ def add_to_menu(window, menu_bar):
     about_menu = tk.Menu(menu_bar, tearoff=0)
     about_menu.add_command(label="Über Homepage", command=lambda:
                            window.show_message("Feature in Arbeit."))
+
+    about_menu.add_command(label="Reload", command=lambda:
+                           window.reload())
+
     menu_bar.add_cascade(label="Info", menu=about_menu)
