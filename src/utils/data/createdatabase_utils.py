939ac4a6152870f87378f91c854f7932618bf90f
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-04-24 13:26:49 +0200

    feat(database): enhance database creation with new tables and indexes

--- src/utils/data/createdatabase_utils.py
+++ src/utils/data/createdatabase_utils.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 import sqlite3
+from utils.data.database_connection import DatabaseConnection
 import config
 
 
-def create_database(path: Path = None) -> None:
+class Error(Exception):
+    """General exception class for database errors."""
+    pass
+
+
+def create_database(db_path: Path = None) -> None:
     def create_transactions_table(cursor, conn):
         try:
             cursor.execute(
@@ -11,7 +17,9 @@ def create_database(path: Path = None) -> None:
                 CREATE TABLE IF NOT EXISTS tbl_Transaction (
                     i8_TransactionID INTEGER PRIMARY KEY AUTOINCREMENT,
                     i8_AccountID INTEGER NOT NULL,
-                    i8_Date INTEGER NOT NULL,  -- Format: YYMMDD
+                    str_Date TEXT NOT NULL,  -- Format: YYYY-MM-DD
+                    str_Bookingdate TEXT NOT NULL,  -- Format: YYYY-MM-DD
+                    i8_TransactionTypeID INTEGER,
                     real_Amount REAL NOT NULL,
                     str_Purpose TEXT NOT NULL,
                     i8_CounterpartyID INTEGER,
@@ -26,7 +34,10 @@ def create_database(path: Path = None) -> None:
                         ON DELETE SET NULL,
                     FOREIGN KEY (i8_AccountID)
                         REFERENCES tbl_Account(i8_AccountID)
-                        ON DELETE CASCADE
+                        ON DELETE CASCADE,
+                    FOREIGN KEY (i8_TransactionTypeID)
+                        REFERENCES tbl_TransactionType(i8_TransactionTypeID)
+                        ON DELETE SET NULL
                 );
                 '''
             )
@@ -116,13 +127,30 @@ def create_database(path: Path = None) -> None:
                 '''
                 CREATE TABLE IF NOT EXISTS tbl_Counterparty (
                     i8_CounterpartyID INTEGER PRIMARY KEY AUTOINCREMENT,
-                    str_CounterpartyName TEXT UNIQUE NOT NULL
+                    str_CounterpartyName TEXT UNIQUE NOT NULL,
+                    str_CounterpartyNumber TEXT UNIQUE NOT NULL
                 );
                 '''
             )
             conn.commit()
         except sqlite3.Error as e:
             print("[Fehler] Tabelle 'tbl_Counterparty' konnte"
+                  f"nicht erstellt werden: {e}")
+
+    def create_transaction_typ_table(cursor, conn):
+        try:
+            cursor.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS tbl_TransactionTyp (
+                    i8_TransactionTypID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    str_TransactionTypName TEXT NOT NULL,
+                    str_TransactionTypNumber TEXT NOT NULL
+                );
+                '''
+            )
+            conn.commit()
+        except sqlite3.Error as e:
+            print("[Fehler] Tabelle 'tbl_TransactionTyp' konnte"
                   f"nicht erstellt werden: {e}")
 
     def create_account_table(cursor, conn):
@@ -133,7 +161,9 @@ def create_database(path: Path = None) -> None:
                 str_AccountName TEXT UNIQUE NOT NULL,
                 str_AccountNumber TEXT,
                 real_AccountBalance REAL DEFAULT 0.0,
-                real_AccountDifference REAL DEFAULT 0.0
+                real_AccountDifference REAL DEFAULT 0.0,
+                i8_RecordDate INTEGER,
+                i8_ChangeDate INTEGER
                 );
             ''')
             conn.commit()
@@ -141,28 +171,109 @@ def create_database(path: Path = None) -> None:
             print("[Fehler] Tabelle 'tbl_Account' konnte"
                   f"nicht erstellt werden: {e}")
 
+    def create_account_history_table(cursor, conn):
+        try:
+            cursor.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS tbl_AccountHistory (
+                    i8_AccountHistoryID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    i8_AccountID INTEGER NOT NULL,
+                    real_Balance REAL,
+                    str_RecordDate TEXT NOT NULL,
+                    str_ChangeDate TEXT NOT NULL,
+                    FOREIGN KEY (i8_AccountID)
+                        REFERENCES tbl_Account(i8_AccountID)
+                        ON DELETE CASCADE
+                );
+                '''
+            )
+            conn.commit()
+        except sqlite3.Error as e:
+            print("[Fehler] Tabelle 'tbl_AccountHistory' konnte nicht"
+                  "erstellt werden:", e)
+
+    def create_indexes_for_transaction_table(cursor) -> None:
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_transaction_account_date
+            ON tbl_Transaction(i8_AccountID, str_Date);
+        ''')
+        # cursor.execute('''
+        #     CREATE INDEX IF NOT EXISTS idx_transaction_bookingdate
+        #     ON tbl_Transaction(str_Bookingdate);
+        # ''')
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_transaction_category
+            ON tbl_Transaction(i8_AccountID, i8_CategoryID, str_Date);
+        ''')
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_transaction_counterparty
+            ON tbl_Transaction(i8_CounterpartyID, str_Date);
+        ''')
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_transaction_counterparty
+            ON tbl_Transaction(i8_AccountID, i8_CounterpartyID, str_Date);
+        ''')
+
+    def create_indexes_for_account_history_table(cursor) -> None:
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_accounthistory_account
+            ON tbl_AccountHistory(i8_AccountID);
+        ''')
+
+    def create_indexes_for_category_table(cursor) -> None:
+        cursor.execute('''
+            CREATE INDEX IF NOT EXISTS idx_category_budgetperiod
+            ON tbl_Category(i8_BudgetPeriodID);
+        ''')
+
+    def create_all_indexes(cursor, conn) -> None:
+        create_indexes_for_transaction_table(cursor)
+        create_indexes_for_account_history_table(cursor)
+        create_indexes_for_category_table(cursor)
+        conn.commit()
+
     try:
-        db_location = path if path else config.Database.PATH
-        db_location.parent.mkdir(parents=True, exist_ok=True)
-        conn = sqlite3.connect(db_location)
+        db_path = db_path if db_path else config.Database.PATH
+        db_path.parent.mkdir(parents=True, exist_ok=True)
+        conn = DatabaseConnection.get_connection(db_path)
         cursor = conn.cursor()
 
         # Tabellen erstellen
         create_category_table(cursor, conn)
         create_counterparty_table(cursor, conn)
         create_account_table(cursor, conn)
+        create_account_history_table(cursor, conn)
+        create_transaction_typ_table(cursor, conn)
         create_transactions_table(cursor, conn)
 
-        print(f'Datenbank wurde erstellt unter: {db_location}')
+        print(f'Database created successfully: {db_path}')
 
+        # Indizes erstellen
+        create_all_indexes(cursor, conn)
+        print("Indexes were created successfully.")
     except sqlite3.Error as e:
         print(f"[Fehler] Datenbankverbindung fehlgeschlagen: {e}")
 
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
-        if 'conn' in locals():
-            conn.close()
+
+def delete_database(path: Path = None) -> None:
+    """Delete the database file.
+
+    Args:
+        path (Path): Path to the database file.
+
+    Raises:
+        FileNotFoundError: If the database file does not exist.
+        Exception: If there is an error deleting the database file.
+    """
+    db_path = path if path else config.Database.PATH
+    try:
+        DatabaseConnection.close_connection()
+        db_path.unlink()
+        print(f'Datenbank wurde gelöscht: {db_path}')
+    except FileNotFoundError:
+        raise Error(f'Datenbank nicht gefunden: {db_path}')
+    except Exception as e:
+        raise Error(f"[Fehler] Datenbank konnte nicht gelöscht werden: {e}")
 
 
 if __name__ == '__main__':
