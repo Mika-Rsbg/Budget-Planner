594fc0c32370430dd7e4d99cb7d5554d90277d07
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-05-01 09:13:14 +0200

    fix(main): add type hint to main function for improved clarity

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from gui.homepage.homepage import Homepage
 from utils.data.createdatabase_utils import create_database
 
 
-def main():
+def main() -> None:
     create_database()
     app = Homepage(fullscreen=False)
     app.run()
