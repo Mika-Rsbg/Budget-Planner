bf1d98bc016134265a7821569f064fdcf5cdb093
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-05-01 09:12:13 +0200

    fix(database_connection): add default db_path argument and improve docstrings for connection methods

--- src/utils/data/database_connection.py
+++ src/utils/data/database_connection.py
@@ -1,6 +1,7 @@
 import sqlite3
 from pathlib import Path
 from typing import Optional
+import config
 
 
 class DatabaseConnection:
@@ -8,13 +9,37 @@ class DatabaseConnection:
     _cursor: Optional[sqlite3.Cursor] = None
 
     @staticmethod
-    def get_connection(db_path: Path) -> sqlite3.Connection:
+    def get_connection(
+        db_path: Path = config.Database.PATH
+    ) -> sqlite3.Connection:
+        """
+        Returns a singleton instance of the database connection.
+        If the connection does not exist, it creates a new one.
+        If the connection already exists, it returns the existing one.
+
+        Args:
+            db_path (Path): The path to the database file.
+        Returns:
+            sqlite3.Connection: The database connection instance.
+        """
         if DatabaseConnection._instance is None:
             DatabaseConnection._instance = sqlite3.connect(db_path)
         return DatabaseConnection._instance
 
     @staticmethod
-    def get_cursor(db_path: Path) -> sqlite3.Cursor:
+    def get_cursor(
+        db_path: Path = config.Database.PATH
+    ) -> sqlite3.Cursor:
+        """
+        Returns a singleton instance of the database cursor.
+        If the cursor does not exist, it creates a new one.
+        If the cursor already exists, it returns the existing one.
+
+        Args:
+            db_path (Path): The path to the database file.
+        Returns:
+            sqlite3.Cursor: The database cursor instance.
+        """
         if DatabaseConnection._cursor is None:
             DatabaseConnection._cursor = DatabaseConnection.get_connection(
                 db_path
@@ -22,12 +47,20 @@ class DatabaseConnection:
         return DatabaseConnection._cursor
 
     def close_cursor() -> None:
+        """
+        Closes the database cursor if it exists.
+        Sets the cursor instance to None after closing.
+        """
         if DatabaseConnection._cursor:
             DatabaseConnection._cursor.close()
             DatabaseConnection._cursor = None
 
     @staticmethod
-    def close_connection():
+    def close_connection() -> None:
+        """
+        Closes the database connection if it exists.
+        Sets the connection instance to None after closing.
+        """
         if DatabaseConnection._instance:
             DatabaseConnection.close_cursor()
             DatabaseConnection._instance.close()
