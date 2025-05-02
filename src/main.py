1dade8d03dc605ec25428ed94ce40cd7857fd0b3
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-05-02 20:26:38 +0200

    fix(main): update docstring for clarity on application execution

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from utils.data.createdatabase_utils import create_database
 
 
 def main() -> None:
+    """
+    Main function to run the application.
+    It creates the database and runs the homepage application.
+    """    
     create_database()
     app = Homepage(fullscreen=False)
     app.run()
