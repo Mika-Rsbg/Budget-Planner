e3134a9d580b59b343d6ebc1e2e2fa067ded2e21
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-05-02 20:34:52 +0200

    fix(main): correct indentation in docstring for main function

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ def main() -> None:
     """
     Main function to run the application.
     It creates the database and runs the homepage application.
-    """    
+    """
     create_database()
     app = Homepage(fullscreen=False)
     app.run()
