c8118f7b102cee04ac11d448aeb1a643d1db5798
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-05-05 16:25:55 +0200

    feat(logging): enhance main function with logging setup and execution tracking

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
-from gui.homepage.homepage import Homepage
-from utils.data.createdatabase_utils import create_database
+import logging
+from utils.logging.logger_config import setup_logging
+from utils.logging.logging_tools import logg
 
 
+logger = logging.getLogger(__name__)
+
+
+@logg
 def main() -> None:
     """
     Main function to run the application.
     It creates the database and runs the homepage application.
     """
+    from gui.homepage.homepage import Homepage
+    from utils.data.createdatabase_utils import create_database
+
+    logger.info("Application started.")
+
     create_database()
+
     app = Homepage(fullscreen=False)
     app.run()
 
 
 if __name__ == "__main__":
+    setup_logging()
     main()
