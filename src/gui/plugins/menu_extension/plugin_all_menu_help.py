aa5e05836f83927b42ca129fb5c5a62c88765d2e
Mika-Rsbg <135967728+Mika-Rsbg@users.noreply.github.com>
2025-08-09 17:09:22 +0000

    feat(menu): update help menu label to indicate WIP status for clarity

--- src/gui/plugins/menu_extension/plugin_all_menu_help.py
+++ src/gui/plugins/menu_extension/plugin_all_menu_help.py
@@ -3,6 +3,6 @@ import tkinter as tk
 
 def add_to_menu(window, menu_bar):
     help_menu = tk.Menu(menu_bar, tearoff=0)
-    help_menu.add_command(label="Hilfe", command=lambda:
+    help_menu.add_command(label="Hilfe (WIP)", command=lambda:
                           window.show_message("Feature in Arbeit."))
     menu_bar.add_cascade(label="Hilfe", menu=help_menu)
